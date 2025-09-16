19f44cb719238a043e9a14d5493bf8bd50db079f
FeliCres7 <48242703@est.ort.edu.ar>
2025-09-15 23:43:32 -0300

    .

--- landing/src/App.js
+++ landing/src/App.js
@@ -28,39 +28,47 @@ function App() {
             <div className='div2'>
                 <h1>Todo lo que necesitas en una app</h1>
                 <h2 className='h2div2'>Herramientas completas para el cuidado y protección de tu piel, respaldadas por tecnología médica avanzada.</h2>
-                <div>
+                <div className='divarribadiv2'><div className='divdiv2'> 
+                    <img className='imgdiv2' src="/iconcamara.png" alt="" />
                     <h2>Análisis de lunares con IA</h2>
                     <p>Fotografía tus lunares y recibe un análisis instantáneo de riesgo utilizando algoritmos de deep learning.</p>
                 </div>
-                <div>
+                <div className='divdiv2'>
+                    <img className='imgdiv2' src="/iconstats.png" alt="" />
                     <h2>Evaluación de Factores de Riesgo</h2>
                     <p>Cuestionario completo que evalúa tu historial médico, tipo de piel y exposición solar.</p>
                 </div>
-                <div>
+                <div className='divdiv2'>
+                    <img className='imgdiv2' src="/iconheart.png" alt="" />
                     <h2>Consejos Personalizados</h2>
                     <p>Recomendaciones específicas para el cuidado de tu piel basadas en tu perfil de riesgo</p>
-                </div>
-                <div>
+                </div></div>
+                <div className='divabajodiv2'><div className='divdiv2'>
+                    <img className='imgdiv2' src="/iconidea.png" alt="" />
                     <h2>Tips de prevención</h2>
                     <p>Página con consejos para el cuidado de la piel y la prevención del melanoma</p>
                 </div>
-                <div>
+                <div className='divdiv2'>
+                    <img className='imgdiv2' src="/iconhistory.png" alt="" />
                     <h2>Registro de lunares</h2>
                     <p>Registra y monitorea la evolución de tus lunares a lo largo del tiempo.</p>
-                </div>
+                </div></div>
+                
             </div>
 
-            <div>
+            <div className='div3'>
                 <h1>Cómo funciona DermaScan</h1>
-                <h3>En solo tres pasos simples, obtené un análisis completo de riesgo y recomendaciones personalizadas para el cuidado de tu piel.</h3>
+                <h2>En solo tres pasos simples, obtené un análisis completo de riesgo y recomendaciones personalizadas para el cuidado de tu piel.</h2>
+                <div className='containerdiv3'>
+                <div className='divdiv3'><p className='numdiv3'>1</p><img className='imgdiv3' src="/iconcamara.png" alt="" /><h3>Fotografiá tus Lunares</h3><p className='pdiv3'>Usa la cámara de tu smartphone para capturar imágenes claras de lunares o manchas sospechosas.</p></div>
+                <div className='divdiv3'><p className='numdiv3'>2</p><img className='imgdiv3' src="/iconlupaa.png" alt="" /><h3>Análisis con IA</h3><p className='pdiv3'>Nuestra inteligencia artificial analiza la imagen y evalúa patrones de riesgo en segundos.</p></div>
+                <div className='divdiv3'><p className='numdiv3'>3</p><img className='imgdiv2' src="/iconcircles.png" alt="" /><h3>Recibe Resultados</h3><p className='pdiv3'>Obtén un reporte detallado con nivel de riesgo y recomendaciones médicas específicas.</p></div>
+                </div>
 
-                <div><p>1</p><h2>Fotografiá tus Lunares</h2><p>Usa la cámara de tu smartphone para capturar imágenes claras de lunares o manchas sospechosas.</p></div>
-                <div><p>2</p><h2>Análisis con IA</h2><p>Nuestra inteligencia artificial analiza la imagen y evalúa patrones de riesgo en segundos.</p></div>
-                <div><p>3</p><h2>Recibe Resultados</h2><p>Obtén un reporte detallado con nivel de riesgo y recomendaciones médicas específicas.</p></div>
             </div>
 
-            <div><div>
-                <h1>¿Por Qué es Tan Importante la Detección Temprana?</h1>
+            <div className='div4'><div>
+                <h1 className='div4h1'>¿Por Qué es Tan Importante la Detección Temprana?</h1>
                 <h3>El melanoma es la forma más peligrosa de cáncer de piel, pero cuando se detecta tempranamente, tiene una
                     tasa de supervivencia del 99%. La prevención y detección oportuna pueden salvar vidas.</h3>
                 <ul>
